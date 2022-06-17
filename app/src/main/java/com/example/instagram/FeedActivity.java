b4fab1035882214a997bac0a116ad1aa5fb2e0c4
neysapatel <patelneysa@gmail.com>
2022-06-17 11:25:08 -0700

    Implemented infinite scrolling

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -6,11 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
@@ -24,6 +26,7 @@ public class FeedActivity extends AppCompatActivity {
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
     private SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +39,9 @@ public class FeedActivity extends AppCompatActivity {
         adapter = new PostsAdapter(this, allPosts);
 
         rvPosts.setAdapter(adapter);
-        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvPosts.setLayoutManager(linearLayoutManager);
         queryPosts();
 
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
@@ -51,6 +56,29 @@ public class FeedActivity extends AppCompatActivity {
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
+
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+                query.include(Post.KEY_USER);
+                query.addDescendingOrder("createdAt");
+                query.findInBackground(new FindCallback<Post>() {
+                    @Override
+                    public void done(List<Post> posts, ParseException e) {
+                        if (e != null) {
+                            return;
+                        }
+
+                        allPosts.addAll(posts);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        };
+
+        rvPosts.addOnScrollListener(scrollListener);
     }
 
     public void fetchTimelineAsync(int page) {
